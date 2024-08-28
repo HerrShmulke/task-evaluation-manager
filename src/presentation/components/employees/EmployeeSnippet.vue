dd80a82c8ef36f1906ce5b9b3c55abdff06f105d
Aleksey <shmulter@mail.ru>
2024-08-29 04:13:33 +0700

    feat(employee): add employee view snippet

---
+++ src/presentation/components/employees/EmployeeSnippet.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import IconButton from '../buttons/IconButton.vue';
+import VText from '../VText/VText.vue';
+
+interface IProps {
+  fullName: string;
+}
+
+interface IEmits {
+  (e: 'delete'): void;
+  (e: 'edit'): void;
+}
+
+const props = defineProps<IProps>();
+const emit = defineEmits<IEmits>();
+
+function onEdit() {
+  emit('edit');
+}
+
+function onDelete() {
+  emit('delete');
+}
+</script>
+
+<template>
+  <div class="employee-snippet">
+    <VText size="medium">{{ props.fullName }}</VText>
+
+    <div class="employee-snippet__actions">
+      <IconButton name="pencil" color="ghost" size="small" @click="onEdit" />
+      <IconButton name="trash" color="danger" size="small" @click="onDelete" />
+    </div>
+  </div>
+</template>
+
+<style>
+.employee-snippet {
+  padding: 16px 20px;
+  border: 1px solid transparent;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.employee-snippet:hover {
+  border: 1px solid var(--blue);
+}
+
+.employee-snippet__actions {
+  display: flex;
+  column-gap: 8px;
+}
+</style>
