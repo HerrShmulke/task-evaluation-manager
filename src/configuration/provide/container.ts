11cffae9b43f6f2ee93fda5fc5e79615956a9d08
Aleksey <shmulter@mail.ru>
2024-08-29 02:55:21 +0700

    feat(employee): add employee service to inject container

--- src/configuration/provide/container.ts
+++ src/configuration/provide/container.ts
@@ -3,10 +3,13 @@ import { injectionKeys } from "./injection-keys";
 import { InjectionKey } from "vue";
 import { ProjectResource } from "@/infrastructure/project/project-resource";
 import { RouteService } from "@/infrastructure/services/route-service/route-service";
+import { EmployeeService } from "@/application/employee/services/employee-service";
+import { EmployeeResource } from "@/infrastructure/employee/employee-resource";
 
 type ProvideFn = (key: InjectionKey<unknown>, value: unknown) => void;
 
 export function bindServices(provide: ProvideFn) {
   provide(injectionKeys.projectService, new ProjectService(new ProjectResource()));
+  provide(injectionKeys.employeeService, new EmployeeService(new EmployeeResource()));
   provide(injectionKeys.routeService, new RouteService());
 }
--- src/configuration/provide/injection-keys.ts
+++ src/configuration/provide/injection-keys.ts
@@ -1,8 +1,10 @@
+import { IEmployeeService } from '@/application/employee/services/employee-service.interface';
 import { IProjectService } from '@/application/project/services/project-service.interface';
 import { IRouteService } from '@/infrastructure/services/route-service/route-service.interface';
 import { InjectionKey } from 'vue';
 
 export const injectionKeys = {
   projectService: Symbol('projectService') as InjectionKey<IProjectService>,
-  routeService: Symbol('routeService') as InjectionKey<IRouteService>
+  employeeService: Symbol('employeeService') as InjectionKey<IEmployeeService>,
+  routeService: Symbol('routeService') as InjectionKey<IRouteService>,
 } as const;
