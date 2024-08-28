fdabe1a4a7108398b7b5d6ac40c172803284152c
Aleksey <shmulter@mail.ru>
2024-08-29 02:59:08 +0700

    feat(employee): add delete mutation query

---
+++ src/infrastructure/employee/queries/useDeleteEmployeeMutation.ts
@@ -0,0 +1,26 @@
+import { useMutation, useQueryClient } from "@tanstack/vue-query";
+import { employeeQueryKey } from "../constants";
+import { injectionKeys } from "@/configuration/provide/injection-keys";
+import { inject } from "vue";
+import { EmployeeDTO } from "@/application/employee/employee-dto";
+
+export function useDeleteEmployeeMutation() {
+  const employeeService = inject(injectionKeys.employeeService)!;
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationKey: [employeeQueryKey],
+    mutationFn: (employee: EmployeeDTO) => {
+      if (employee.id !== undefined) {
+        return employeeService.delete(employee.id)
+      }
+
+      return Promise.resolve();
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [employeeQueryKey],
+      })
+    }
+  });
+}
