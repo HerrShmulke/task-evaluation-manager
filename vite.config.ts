404d68e59b88229deca9f5a467e04215c531f5b7
Aleksey <shmulter@mail.ru>
2024-08-30 01:23:26 +0700

    chore: setup vite eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+// @ts-ignore
+import eslint from 'vite-plugin-eslint';
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), eslint()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
